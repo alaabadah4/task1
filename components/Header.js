8276d570f088951cc8dcced1a2a13fa04055526f
alaabadah4 <alaabadah4@gmail.com>
2023-04-29 19:56:15 +0300

    task commit

---
+++ components/Header.js
@@ -0,0 +1,37 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ *
+ * @format
+ */
+
+import React from 'react';
+import { StyleSheet, Text, View } from 'react-native';
+ const Header =( {title})=>{
+  return(
+    <View style={styles.header}>
+    <Text style={styles.text}>
+      {title}
+    </Text>
+  </View>
+  )
+ };
+
+ Header.defaultProps={
+  title:'Shoping List'
+ }
+
+ 
+export default Header;
+const styles = StyleSheet.create({
+  header:{
+    height: 60,
+    padding: 15,
+    backgroundColor: "darkslateblue"
+  },
+  text:{
+    color:'#fff',
+     fontSize:23,
+     textAlign:'center'
+  }
+ })
